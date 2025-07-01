a16749f1852eac627cb839c6cc72aef41d0fc1d2
Chandan Y S <chandan@briisk.io>
2025-07-01 20:31:57 +0530

    Implement Chatbot functionality with state management and client integration; add ChatMessage and ChatState models

---
+++ app.py
@@ -0,0 +1,48 @@
+import asyncio
+import uuid
+
+from src.states.chat_state import ChatState
+from src.clients.client import Client
+
+class Chatbot:
+    def __init__(self, user_id: str):
+        self.chat_state = ChatState(messages = [], user_id = user_id)
+        self.client = Client()
+        
+    async def chat(self):
+        while True:
+            user_message = input("Enter your message: ")
+        
+            if user_message.lower() == "exit":
+                print("========== Chat ended ==========")
+                break
+            
+            self.chat_state["messages"].append({
+                "role": "user",
+                "message": user_message,
+            })
+            
+            print(f"=========== User: {self.chat_state["messages"]} ===========")
+            
+            respose = await self.client.main(
+                user_id = self.chat_state["user_id"],
+                thread_id = str(uuid.uuid4()),
+                chat_messages = self.chat_state["messages"]
+            )
+            
+            print(f"=========== Assistant: {respose} ===========")
+            
+            self.chat_state["messages"].append({
+                "role": "assistant",
+                "message": respose,
+            })
+            
+        print("Chat history:")
+        for msg in self.chat_state["messages"]:
+            print(f"{msg['role']}: {msg['message']}")
+            
+if __name__ == "__main__":
+    user_id = input("Enter your user ID: ")
+    chatbot = Chatbot(user_id)
+    
+    asyncio.run(chatbot.chat())
---
+++ src/clients/client.py
@@ -0,0 +1,70 @@
+import asyncio
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import SystemMessage
+from src.memories.memory import Memory
+from src.prompts.prompt import Prompt
+from src.states.chat_state import ChatMessage
+from typing import List
+
+load_dotenv()
+
+class Client:
+    async def main(self, user_id: str, thread_id: str, chat_messages: List[ChatMessage]) -> str:
+        client = MultiServerMCPClient(
+        connections=
+        {
+                # "math": {
+                #     "command": "python",
+                #     "args": ["./src/servers/mathserver.py"], # Ensure this is the correct path to your math_server.py file
+                #     "transport": "stdio",
+                # },
+                # "weather": {
+                #     "url": "http://localhost:8000/mcp",
+                #     "transport": "streamable_http",
+                # },
+                "azure": {
+                    "command": "npx",
+                    "args": ["-y", "@azure/mcp@latest", "server", "start"],
+                    "env": None,
+                    "transport": "stdio"
+                }
+        } 
+        )
+        
+        # Get the tools available from the MCP client
+        tools = await client.get_tools()
+        
+        # Initialize the memory with user and thread IDs
+        memory = Memory(user_Id=user_id, thread_Id=thread_id)
+        memory.init_az_memory()
+        az_memories = memory.get_az_memories()
+        
+        # Get the system prompt with the Azure memories
+        system_prompt = Prompt().get_system_prompt(az_memories)
+        
+        # Build the system message with the available tools and memories
+        system_message = SystemMessage(content=f"{system_prompt}")
+        
+        # Initialize the chat model
+        # Ensure you have the correct model name and configuration for your Azure OpenAI instance
+        llm = init_chat_model("azure_openai:gpt-4o-mini")
+        
+        # Create the React agent with the chat model, tools, and system message
+        agent = create_react_agent(
+            llm,
+            tools,
+            prompt=system_message,
+        )
+        
+        # Invoke the agent with a user message
+        response = await agent.ainvoke(
+            input = chat_messages[-1],  # Use the last user message as input
+        )
+        
+        print(response["messages"][-1].content)
+        
+        
